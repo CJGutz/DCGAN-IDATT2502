a44262398c205dac78086907d1037c2bf7c92b36
tomasbera <tomas.beranek02@gmail.com>
2023-10-27 17:03:57 +0200

    added the method for printing the losses of D(x) and G(x)

--- Visualization.py
+++ Visualization.py
@@ -1,5 +1,6 @@
 # graf som ser på D(x) loss og G(x) loss
 # printe ut bilder etter hver epoch og på starten (2 forskjllige metoder)
+import matplotlib.pyplot as plt
 
 def print_start_img():
     return 0
@@ -11,4 +12,11 @@ def print_epoch_img():
 
 # method that shows Generator and Discriminator loss during training
 def print_loss(G_loss, D_loss):
-    return 0
+    plt.figure(figsize=(10, 5))
+    plt.title("Generator and Discriminator Loss")
+    plt.plot(G_loss, label="G")
+    plt.plot(D_loss, label="D")
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
