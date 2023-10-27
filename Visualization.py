3fdf1ff8a25b6f22d89fbba23eafee6c860505c8
tomasbera <tomas.beranek02@gmail.com>
2023-10-27 16:06:51 +0200

    added skeleton for needed methods

--- Visualization.py
+++ Visualization.py
@@ -1,2 +1,14 @@
 # graf som ser på D(x) loss og G(x) loss
 # printe ut bilder etter hver epoch og på starten (2 forskjllige metoder)
+
+def print_start_img():
+    return 0
+
+
+def print_epoch_img():
+    return 0
+
+
+# method that shows Generator and Discriminator loss during training
+def print_loss(G_loss, D_loss):
+    return 0
