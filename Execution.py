55e75a991537cfe4982955e0d26fcdb6583b5d6c
tomasbera <tomas.beranek02@gmail.com>
2023-10-28 15:07:54 +0200

    refactor: dataloader method now more readable

--- Execution.py
+++ Execution.py
@@ -10,55 +10,48 @@ from tqdm import tqdm
 
 # Define Visualization class (in Visualization.py)
 from Visualization import print_start_img
+# Define DCGAN class (in DCGAN.py)
+from DCGAN import DCGAN as gan
 
 
-def data_loader(dataset_numb, image_size, batch_size):
-    ds_root = "./datasets"
-    extract_path = f'{ds_root}/celeba-dataset'
+def download_and_extract_zip(zip_file_path, extract_path, desc='Extracting files'):
+    # If dateset isn't in the directory its downloaded from the zipfile
+    if not os.path.isdir(extract_path):
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
 
+            # Get the total number of files in the zip archive
+            total_files = len(zip_ref.namelist())
+            pbar = tqdm(total=total_files, desc=desc)
+
+            # Extract each file and update the progress bar
+            for file in zip_ref.namelist():
+                zip_ref.extract(file, extract_path)
+                pbar.update(1)
+            pbar.close()
+
+
+def data_loader(dataset_numb, image_size, batch_size, ds_root="./datasets"):
     if dataset_numb == 1:
         # number of color channels, since its grey scaling, 1 is need, and not 3 (RGB)
         nc = 1
         # needed since the dataset is differently put than the others
-        transform = transforms.Compose([transforms.Resize(image_size), transforms.CenterCrop(image_size),
-                                        transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+        transform = transforms.Compose([transforms.Resize(image_size), transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
         dataset = torchvision.datasets.MNIST(root=ds_root, train=True, download=True, transform=transform)
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
-        print_start_img(dataloader)
-        return dataloader, nc
+    else:
+        # rest of datasets have 3 color channels
+        nc = 3
+        transform = transforms.Compose([transforms.Resize(image_size), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    # rest of datasets have 3 color channels
-    nc = 3
-    transform = transforms.Compose([transforms.Resize(image_size), transforms.CenterCrop(image_size),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), ])
-
-    if dataset_numb == 2:
-        # If dateset isn't in the directory its downloaded from the zipfile
-        if not os.path.isdir(extract_path):
-            # Specify the path to the manually uploaded datasets zip file
+        if dataset_numb == 2:
             zip_file_path = f'{ds_root}/img_align_celeba.zip'
+            extract_path = f'{ds_root}/celeba-dataset'
+            download_and_extract_zip(zip_file_path, extract_path)
 
-            # Get the total number of files in the zip archive
-            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-                total_files = len(zip_ref.namelist())
+            dataset = dset.ImageFolder(root=extract_path, transform=transform)
 
-            # Create a progress bar using tqdm
-            pbar = tqdm(total=total_files, desc='Extracting files')
-            # Extract each file and update the progress bar
-            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-                for file in zip_ref.namelist():
-                    zip_ref.extract(file, extract_path)
-                    pbar.update(1)
-            pbar.close()
+        elif dataset_numb == 3:
+            dataloader = 3
 
-        dataset = dset.ImageFolder(root=extract_path,
-                                   transform=transform)
-
-    elif dataset_numb == 3:
-        dataset = 3
-
-    # Create the dataloader
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
     print_start_img(dataloader)
 
@@ -80,4 +73,4 @@ def run(dataset_numb, img_size, batch_size):
 
 
 if __name__ == "__main__":
-    run(1, 64, 32)
+    run(2, 64, 32)
