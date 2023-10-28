51e8094599a4a0cac637d5af2bdf6b3246cb970a
tomasbera <tomas.beranek02@gmail.com>
2023-10-28 10:16:54 +0200

    Added the dataloader method, and added possibility for multiple datasets

--- Execution.py
+++ Execution.py
@@ -1,11 +1,65 @@
 # metode run() som kjører programmet, metode for å laste in dataset
+import os
+import zipfile
+import tqdm
+import torch
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+from tqdm import tqdm
 
-def data_loader():
-    # implement multiple datasets
-    return 1
+# Define Visualization class (in Visualization.py)
+from Visualization import print_start_img
+
+
+def data_loader(dataset_numb, image_size, batch_size, channels):
+    ds_root = "./datasets"
+    extract_path = f'{ds_root}/celeba-dataset'
+
+    if dataset_numb == 1:
+        # If dateset isn't in the directory its downloaded from the zipfile
+        if not os.path.isdir(extract_path):
+            # Specify the path to the manually uploaded datasets zip file
+            zip_file_path = f'{ds_root}/img_align_celeba.zip'
+
+            # Get the total number of files in the zip archive
+            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+                total_files = len(zip_ref.namelist())
+
+            # Create a progress bar using tqdm
+            pbar = tqdm(total=total_files, desc='Extracting files')
+            # Extract each file and update the progress bar
+            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+                for file in zip_ref.namelist():
+                    zip_ref.extract(file, extract_path)
+                    pbar.update(1)
+            pbar.close()
+
+        dataset = dset.ImageFolder(root=extract_path,
+                                   transform=transforms.Compose([
+                                       transforms.Resize(image_size),
+                                       transforms.CenterCrop(image_size),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                   ]))
+
+    elif dataset_numb == 2:
+        dataset = 2
+
+    elif dataset_numb == 3:
+        dataset = 3
+
+    # Create the dataloader
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    # Plot some training images
+    print_start_img(dataloader)
+
+    return dataloader
 
 
 def run():
+    dataset_numb = 1
+
     img_size = 64
     beta1 = 0.5
     lr = 0.0002
