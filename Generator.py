09c7ca93921ea41cafa3eb603a2e9ac61d9166e3
tomasbera <tomas.beranek02@gmail.com>
2023-10-30 09:43:38 +0100

    refacor: smal changes to code structure

--- Generator.py
+++ Generator.py
@@ -18,21 +18,20 @@ class Generator(nn.Module):
             nn.ReLU(inplace=True)
         )
 
-        common_relu = nn.ReLU(inplace=True)
-
         for i in range(number_of_layers):
             self.main.add_module(
-                f'conv_trans_{i}',
+                f'conv_{i}',
                 nn.ConvTranspose2d(first_out_channel * 2 ** i,
                                    first_out_channel * 2 ** (i + 1),
                                    kernel_size=4, stride=2, padding=1, bias=False)
             )
-            self.main.add_module(f'batch_norm_{i}',
-                                 nn.BatchNorm2d(first_out_channel * 2 ** (i + 1)))
-            self.main.add_module(f'relu_{i}', common_relu)
+            self.main.add_module(
+                nn.BatchNorm2d(first_out_channel * 2 ** (i + 1))
+            )
+            self.main.add_module(nn.ReLU(inplace=True))
 
         # Output layer
-        self.main.add_module('conv_trans_out',
+        self.main.add_module('conv_out',
                              nn.ConvTranspose2d(first_out_channel * 2 ** number_of_layers,
                                                 output_channels,
                                                 kernel_size=4, stride=2, padding=1, bias=False))
