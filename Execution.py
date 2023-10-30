157e05112b607bc3f8be7038346278153282c465
tomasbera <tomas.beranek02@gmail.com>
2023-10-30 09:12:49 +0100

    feat: added an generator based on Alec Radford documentation

--- Execution.py
+++ Execution.py
@@ -12,6 +12,8 @@ from tqdm import tqdm
 from Visualization import print_start_img
 # Define DCGAN class (in DCGAN.py)
 from DCGAN import DCGAN as dcgan
+# Define Discriminator class (in Discriminator.py)
+from Discriminator import Discriminator as netD
 
 
 def download_and_extract_zip(zip_file_path, extract_path, desc='Extracting files'):
@@ -35,12 +37,14 @@ def data_loader(dataset_numb, image_size, batch_size, ds_root="./datasets"):
         # number of color channels, since its grey scaling, 1 is need, and not 3 (RGB)
         nc = 1
         # needed since the dataset is differently put than the others
-        transform = transforms.Compose([transforms.Resize(image_size), transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+        transform = transforms.Compose(
+            [transforms.Resize(image_size), transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
         dataset = torchvision.datasets.MNIST(root=ds_root, train=True, download=True, transform=transform)
     else:
         # rest of datasets have 3 color channels
         nc = 3
-        transform = transforms.Compose([transforms.Resize(image_size), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        transform = transforms.Compose([transforms.Resize(image_size), transforms.ToTensor(),
+                                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
         if dataset_numb == 2:
             zip_file_path = f'{ds_root}/img_align_celeba.zip'
@@ -57,11 +61,17 @@ def data_loader(dataset_numb, image_size, batch_size, ds_root="./datasets"):
 
     return dataloader, nc
 
+
 def run(dataset_numb, img_size, batch_size, numb_epochs):
     dataset_numb = dataset_numb
     img_size = img_size
     numb_epochs = numb_epochs
     batch_size = batch_size
+
+    beta1 = 0.5
+    lr = 0.0002
+    ndf = 64
+    number_of_layers = 4
 
     # Check if CUDA (GPU support) is available
     if torch.cuda.is_available():
@@ -72,14 +82,13 @@ def run(dataset_numb, img_size, batch_size, numb_epochs):
         gpu_count = 0
         print("CUDA is not available on this system.")
 
-    # flyttes til GAN-klassen etter hvert
-    beta1 = 0.5
-    lr = 0.0002
-
     dataloader, channel_number = data_loader(dataset_numb, img_size, batch_size)
 
     # Decide which device we want to run on
     device = torch.device("cuda:0" if (torch.cuda.is_available() and gpu_count > 0) else "cpu")
+
+    # Create an instance of discriminator and generator
+    discriminator = netD(channel_number, ndf, number_of_layers)
 
     # Create an instance of the dcgan
     gan = dcgan(numb_epochs, dataloader, channel_number, device, batch_size, lr, beta1)
@@ -87,4 +96,4 @@ def run(dataset_numb, img_size, batch_size, numb_epochs):
 
 
 if __name__ == "__main__":
-    run(2, 64, 32)
+    run(2, 64, 32, 5)
--- Generator.py
+++ Generator.py
@@ -1,3 +1,41 @@
-# CNN laget for Generatoren basert på fagartikelen til Alec Radford
-class Generator:
-    pass
+import torch
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self,
+                 noise_dim: int,
+                 first_out_channel: int,
+                 output_channels: int,
+                 number_of_layers: int = 3,
+                 **kwargs):
+        super().__init__(**kwargs)
+
+        self.main = nn.Sequential(
+            # Project and reshape the noise vector
+            nn.Linear(noise_dim, first_out_channel * 4 * 4 * 4),
+            nn.Unflatten(1, (first_out_channel, 4, 4)),
+            nn.ReLU(inplace=True)
+        )
+
+        common_relu = nn.ReLU(inplace=True)
+
+        for i in range(number_of_layers):
+            self.main.add_module(
+                f'conv_trans_{i}',
+                nn.ConvTranspose2d(first_out_channel * 2 ** i,
+                                   first_out_channel * 2 ** (i + 1),
+                                   kernel_size=4, stride=2, padding=1, bias=False)
+            )
+            self.main.add_module(f'batch_norm_{i}',
+                                 nn.BatchNorm2d(first_out_channel * 2 ** (i + 1)))
+            self.main.add_module(f'relu_{i}', common_relu)
+
+        # Output layer
+        self.main.add_module('conv_trans_out',
+                             nn.ConvTranspose2d(first_out_channel * 2 ** number_of_layers,
+                                                output_channels,
+                                                kernel_size=4, stride=2, padding=1, bias=False))
+
+    def forward(self, x):
+        return torch.tanh(self.main(x))
