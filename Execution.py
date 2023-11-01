ddd20cf7bd6059dc9496cc57d90b54be55ea1636
Carl Gützkow <cjgutzkow@gmail.com>
2023-11-01 09:23:38 +0100

    implement dataloader for any dataset

--- Execution.py
+++ Execution.py
@@ -2,10 +2,8 @@ import os
 import zipfile
 import tqdm
 import torch
-import torchvision
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, argument_validation
 from tqdm import tqdm
 import argparse
 
@@ -26,49 +24,39 @@ def download_and_extract_zip(zip_file_path, extract_path):
                 zip_ref.extract(file, extract_path)
 
 
-def data_loader(dataset_numb, image_size, batch_size, ds_root="./datasets"):
-    if dataset_numb == 1:
-        # number of color channels, since its grey scaling, 1 is need, and not 3 (RGB)
-        nc = 1
-        # needed since the dataset is differently put than the others
-        transform = transforms.Compose([transforms.Resize(
-            image_size),
+def data_loader(dataset_path, image_size, batch_size, channels):
+
+    normalization_args = (0.5 for _ in range(channels))
+
+    transform = transforms.Compose(
+        [transforms.Resize(image_size),
             transforms.ToTensor(),
-            transforms.Normalize((0.5,), (0.5,))])
-        dataset = torchvision.datasets.MNIST(
-            root=ds_root, train=True, download=True, transform=transform)
-    else:
-        # rest of datasets have 3 color channels
-        nc = 3
-        transform = transforms.Compose(
-            [transforms.Resize(image_size),
-             transforms.ToTensor(),
-             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-             ])
+         transforms.Normalize(normalization_args, normalization_args)
+         ])
 
-        if dataset_numb == 2:
-            zip_file_path = f'{ds_root}/img_align_celeba.zip'
-            extract_path = f'{ds_root}/celeba-dataset'
-            download_and_extract_zip(zip_file_path, extract_path)
+    if dataset_path.endswith('.zip'):
+        zip_path = dataset_path
+        dataset_path = os.path.dirname(os.path.realpath(dataset_path))
+        download_and_extract_zip(
+            zip_path, dataset_path)
 
-            dataset = dset.ImageFolder(root=extract_path, transform=transform)
-
-        elif dataset_numb == 3:
-            dataloader = 3
+    dataset = dset.ImageFolder(root=dataset_path, transform=transform)
 
     dataloader = torch.utils.data.DataLoader(
         dataset, batch_size=batch_size, shuffle=True)
     print_start_img(dataloader)
 
-    return dataloader, nc
+    return dataloader
 
 
-def run(dataset_numb):
+def run():
 
     parser = argparse.ArgumentParser(
         prog="DCGAN implementation",
     )
-    parser.add_argument("dataset")
+    parser.add_argument("dataset", type=str)
+    parser.add_argument("-c", "--channels", required=True,
+                        type=int, choices=[1, 3])
     parser.add_argument("-i", "--img-size", default=64, type=int)
     parser.add_argument("-l", "--layers", default=4, type=int)
     parser.add_argument('-b', '--batch-size', default=128, type=int)
@@ -89,23 +77,23 @@ def run(dataset_numb):
         gpu_count = 0
         print("CUDA not available")
 
-    dataloader, channel_number = data_loader(
-        dataset_numb, args.img_size, args.batch_size)
+    dataloader = data_loader(
+        args.dataset, args.img_size, args.batch_size, args.channels)
 
     # Device is based on CUDA available gpu
     device = torch.device("cuda:0" if (
         torch.cuda.is_available() and gpu_count > 0) else "cpu")
 
     # Create an instance of discriminator and generator
-    generator = netG(args.nz, args.ngf, channel_number, args.layers)
-    discriminator = netD(channel_number, args.ndf, args.layers)
+    generator = netG(args.nz, args.ngf, args.channels, args.layers)
+    discriminator = netD(args.channels, args.ndf, args.layers)
 
     # Create an instance of the dcgan
-    gan = dcgan(args.epochs, dataloader, channel_number, device, generator, discriminator,
-                args.batch_size, args.lr, args.beta1, args.nz)
+    gan = dcgan(args.epochs, dataloader, args.channels, device, generator,
+                discriminator, args.batch_size, args.lr, args.beta1, args.nz)
 
     gan.train()
 
 
 if __name__ == "__main__":
-    run(2)
+    run()
